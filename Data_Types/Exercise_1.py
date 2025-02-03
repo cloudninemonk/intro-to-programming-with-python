7192931747ac8a516d0dd6415e2e192a59192a55
Tristan Piat <cloudninemonk@gmail.com>
2025-02-03 17:30:00 +1100

    Updating Data_Types>Exercises

---
+++ Data_Types/Exercise_1.py
@@ -0,0 +1,12 @@
+'True' # type is string and class is str
+False # type is boolean and class is bool
+(1, 2, 3) # type is tuple and class is tuple
+1.5 # type is float and class is float
+[1, 2, 3] #type is list and class is list
+2 # type is integer and class is int
+range(5) # type is range and class is range
+{1, 2, 3} # type is set and class is set
+None # type is nonetype and class is nonetype
+{'foo': 'bar'} # type is dictionary and class is dict
+
+
---
+++ Data_Types/Exercise_3.py
@@ -0,0 +1,9 @@
+pets = {'Asta': 'dog', 'Butterscotch': 'cat', 'Pudding': 'cat', 'Neptune': 'fish', 'Darwin': 'lizard'}
+#alternatively to made it more readable
+pets = {
+    'Asta':         'dog', 
+    'Butterscotch': 'cat', 
+    'Pudding':      'cat', 
+    'Neptune':      'fish', 
+    'Darwin':       'lizard',
+}
